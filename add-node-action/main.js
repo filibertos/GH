89fe1eb16f3675099a0880d0f0f82e27bf0745ec
Filiberto Silvestri <filiberto.silvestri@hotmail.de>
2026-09-12 13:03:38 +0200

    Added base

---
+++ add-node-action/main.js
@@ -0,0 +1,17 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+run();
+
+function run() {
+  try {
+    const argumentA = core.getInput("argument-a");
+    const argumentB = core.getInput("argument-b");
+
+    var result = Number(argumentA) + Number(argumentB);
+    
+    core.setOutput("result", result);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
